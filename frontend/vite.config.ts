22bec9681d1e17acf233e8a5136b970b4fc98e5d
Isuru Sampath <isurudananjaya907@gmail.com>
2026-07-01 03:04:41 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       '/api/v1/tenants': { target: 'http://localhost:80', changeOrigin: true },
       '/api/v1/users': { target: 'http://localhost:80', changeOrigin: true },
       '/api/v1/resources': { target: 'http://localhost:80', changeOrigin: true },
+      '/api/v1/st-resources': { target: 'http://localhost:80', changeOrigin: true },
       '/api/v1/bookings': { target: 'http://localhost:80', changeOrigin: true },
       '/api/v1/notifications': { target: 'http://localhost:80', changeOrigin: true },
       '/uploads': {
